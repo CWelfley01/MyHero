f68addd9fe405972b317b82af0654ed4c4bd0fa4
cwelfley01 <cwelfley01@gmail.com>
2022-06-07 19:44:37 -0400

    styleupdate

--- src/components/pages/herodata.js
+++ src/components/pages/herodata.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 
-
+import style from '../../style/main.scss'
 
 
 
@@ -12,10 +12,11 @@ export default function Hero(props) {
     }
     return (
       <div className="hero">
-          {func()}
-          <div className="portrait">
-            <img src={props.image}/>
-          </div>
+        {func()}
+        <div className="portrait">
+          <img src={props.image}/>
+        </div>
+        <div className="text-wrapper">
           <div className="left-side">
             <h1>{props.name}</h1>
             <h3>{props.relation}</h3>
@@ -25,6 +26,7 @@ export default function Hero(props) {
             <h3>{props.strengths}</h3>
             <h3>{props.weaknesses}</h3>
           </div>
+        </div>
       </div>
         
       );
